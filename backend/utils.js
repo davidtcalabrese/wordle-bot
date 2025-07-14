1fdb1d4befceeba63158515ce05717c829f27a57
David Calabrese <69607423+davidtcalabrese@users.noreply.github.com>
2025-07-14 09:36:34 -0500

    Fix path to words file

--- backend/utils.js
+++ backend/utils.js
@@ -2,10 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 const loadWordsFromFile = () => {
-  console.log("Loading words");
   try {
-    const wordsFilePath = path.join(__dirname, 'words.txt');
-    const fileContent = fs.readFileSync(wordsFilePath, 'utf8');
+    const possibleAnswersFile = path.join(__dirname, 'resources', 'possible-answers.txt');
+    const fileContent = fs.readFileSync(possibleAnswersFile, 'utf8');
     return fileContent.split('\n').map(word => word.trim()).filter(word => word.length === 5);
   } catch (error) {
     console.error('Error reading the words file:', error.message);
